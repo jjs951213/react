e96f776ba1366d74f77c0b535406909c7a0aa610
jjs951213 <jjs951213@naver.com>
2020-09-08 13:34:51 +0900

    2.4.5-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './App.css';
 
 
 function App() {
-  const name = '리액트';
-  return <div>{name === '리액트' && <h1>리액트입니다.</h1>}</div>;
+  const name = undefined;
+  return name;
 }
 
 
 
 export default App;
+
