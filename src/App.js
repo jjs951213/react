d71be8a53f655923208bdd103cab6aede13bcd32
jjs951213 <jjs951213@naver.com>
2020-09-08 16:32:24 +0900

    3.3.6 prototype를 통한 props 검증

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import MyComponent from './MyComponent';
- 
+
+
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent name="React">리액트</MyComponent>;
 };
- 
+
+
+
 export default App;
